06c9718af957efd921c3f655e056b1159cba7072
robmllze <86869208+robmllze@users.noreply.github.com>
2024-07-08 12:34:20 +1000

    Update getPositionAsJson to getGlobalPositionAsJson

--- web/xyz_get_device_info/xyz_get_device_info.js
+++ web/xyz_get_device_info/xyz_get_device_info.js
@@ -9,13 +9,13 @@
 //.title~
 
 import { getNavigatorAsJson } from './src/get_navigator_as_json.js';
-import { getPositionAsJson } from './src/get_position_as_json.js';
+import { getGlobalPositionAsJson } from './src/get_global_position_as_json.js';
 import { getScreenAsJson } from './src/get_screen_as_json.js';
 import { isPwaInstalled } from './src/is_pwa_installed.js';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
 window.getNavigatorAsJson = getNavigatorAsJson;
-window.getPositionAsJson = getPositionAsJson;
+window.getGlobalPositionAsJson = getGlobalPositionAsJson;
 window.getScreenAsJson = getScreenAsJson;
 window.isPwaInstalled = isPwaInstalled;
